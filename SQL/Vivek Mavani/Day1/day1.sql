71ad31dff1fb957fd113b7ddd3bddb64013213b8
vivekmavani <vivekcmavani@gmail.com>
2021-08-11 14:32:48 +0530

    REFS #413 FIXES #413

--- SQL/Vivek Mavani/Day1/day1.sql
+++ SQL/Vivek Mavani/Day1/day1.sql
@@ -1,40 +1,39 @@
-
 CREATE database vivek
 
 USE vivek;
-# 1
+--1
 CREATE table countries
 (
 CountryId int CONSTRAINT countid PRIMARY KEY,
 CountryName varchar(50) not null CONSTRAINT cname CHECK(CountryName IN('Italy','India','China')),
 RegionId decimal(10,0)
 )
-# 2
+--2
 CREATE table JobHistory 
 (
-EmployeeId int CONSTRAINT eid PRIMARY KEY,
+EmployeeId int CONSTRAINT eid PRIMARY KEY IDENTITY(1,1),
 StartDate date not null,
 End_Date date CONSTRAINT enddate CHECK(End_Date LIKE '--/--/----'),
 Job_Id  decimal(2) not null,
 Department_Id  int not null,
 )
-# 3
+--3
 CREATE table jobs  
 (
-JobId  int CONSTRAINT jid PRIMARY KEY ,
+JobId  int CONSTRAINT jid PRIMARY KEY IDENTITY(1,1),
 JobTitle varchar(20) not null DEFAULT ' ', 
 MinSalary  int CONSTRAINT minsalay DEFAULT 8000, 
 MaxSalary  int null
 )
-# 4
+--4
 CREATE TABLE employees
 (
 Employee_Id int  not null CONSTRAINT esid PRIMARY KEY,
 FirstName varchar(20) not null,
 LastName varchar(20) not null,
 Email varchar(30) not null,
-PhoneNumber varchar(9),
-Hire_Date date,
+PhoneNumber varchar(10) CONSTRAINT pn CHECK(PhoneNumber LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+Hire_Date date CONSTRAINT employees_date CHECK(Hire_Date LIKE '--/--/----'),
 Job_Id int  not null CONSTRAINT jsid FOREIGN KEY (Job_Id) REFERENCES jobs(JobId),
 Salary varchar(10),
 Commission int not null,
@@ -72,38 +71,42 @@ INSERT INTO [dbo].[employees]
 GO
 
 
-# assingment
+--Alter statement. 
+1.ALTER TABLE JobHistory ADD CONSTRAINT fk_job_id FOREIGN KEY(job_id)  REFERENCES  jobs(JobId)
+2. ALTER TABLE job_history DROP FOREIGN KEY fk_job_id
+3. ALTER TABLE jobhistory ADD location varchar(20)
+
+--Assignment:
 CREATE TABLE employees
 (
-Employee_Id int  not null CONSTRAINT Employee_Id PRIMARY KEY,
+Employee_Id int  not null CONSTRAINT Employee_Id PRIMARY KEY IDENTITY(1,1),
 FirstName varchar(20) not null,
 LastName varchar(20) not null,
 Email varchar(30) not null,
-PhoneNumber varchar(9),
-Hire_Date date,
+PhoneNumber varchar(9) CONSTRAINT pn CHECK(PhoneNumber LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+Hire_Date date CONSTRAINT employees_date CHECK(Hire_Date LIKE '--/--/----'),
 Salary varchar(10),
 Manager_Id int not null
 )
 
-
 CREATE TABLE commission
 (
 commission_id int  not null CONSTRAINT esid PRIMARY KEY,
-e_id int not null CONSTRAINT e_commission_id FOREIGN KEY (e_id) REFERENCES employees(Employee_Id),
-commission decimal(10,2) not null
+e_id int not null CONSTRAINT e_commission_id FOREIGN KEY (e_id) REFERENCES employees(Employee_Id) ON DELETE CASCADE ON UPDATE CASCADE,
+commission decimal(10,2) not null   
 )
 
 CREATE TABLE car
 (
 car_id int  not null CONSTRAINT car_id PRIMARY KEY,
-e_id int not null CONSTRAINT e_id FOREIGN KEY (e_id) REFERENCES employees(Employee_Id),
+e_id int not null CONSTRAINT e_id FOREIGN KEY (e_id) REFERENCES employees(Employee_Id) ON DELETE CASCADE ON UPDATE CASCADE,
 car_name varchar(20) not null,
-car_number varchar(10) not null,
+car_price money not null,
 )
 
 CREATE TABLE completesales
 (
 cs_id int  not null CONSTRAINT c_completesales_id PRIMARY KEY,
 e_id int not null,
-car_id int not null CONSTRAINT car_completesales_id FOREIGN KEY (car_id) REFERENCES car(car_id)
+car_id int not null CONSTRAINT car_completesales_id FOREIGN KEY (car_id) REFERENCES car(car_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
